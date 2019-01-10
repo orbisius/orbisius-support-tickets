9193555c3769142b6eae1249e3e159cc0441a4d6
Slavi <slavi@orbisius.com>
2019-01-10 18:52:19 +0200

    aaa

--- modules/core/shortcodes.php
+++ modules/core/shortcodes.php
@@ -17,17 +17,23 @@ class Orbisius_Support_Tickets_Module_Core_Shortcodes extends Orbisius_Support_T
 	function registerCodes() {
 //		add_shortcode('orbisius_support_list_tickets', [ $this, 'renderTickets' ] );
 		add_shortcode('orbisius_support_submit_ticket', [ $this, 'renderSubmitTicketForm' ] );
-
-
 	}
+
+	private $defaults = [
+        'subject' => '',
+        'message' => '',
+    ];
 
 	/**
 	 * Processes [orbisius_support_submit_ticket] shortcode
 	 * @return string
 	 */
-	public static function renderSubmitTicketForm($attribs = []) {
+	public function renderSubmitTicketForm($attribs = []) {
 		ob_start();
 		$id = 0;
+
+		$sanitized_data = $this->getData();
+		$data = array_replace_recursive($this->defaults, $sanitized_data);
 //
 //		$ad_id = q('ad_id', 0);
 //
@@ -38,6 +44,8 @@ class Orbisius_Support_Tickets_Module_Core_Shortcodes extends Orbisius_Support_T
 var_dump($_REQUEST);
 
 		$ctx = [];
+
+		$row_num = apply_filters('orbisius_support_tickets_filter_submit_ticket_form_message_row_num', 4);
 		?>
 
 		<div id="orbisius_support_tickets_submit_ticket_wrapper" class="orbisius_support_tickets_submit_ticket_wrapper">
@@ -63,7 +71,20 @@ var_dump($_REQUEST);
                             <input name="orbisius_support_tickets_data[subject]"
                                    id="orbisius_support_tickets_data_subject"
                                    type="text" placeholder="Subject"
-                                   class="form-control">
+                                   value="<?php esc_attr_e($data['subject']); ?>"
+                                   class="form-control orbisius_support_tickets_data_subject" />
+                        </div>
+                    </div>
+
+                    <!-- Message body -->
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="orbisius_support_tickets_data_message">Your message</label>
+                        <div class="col-md-9">
+                            <textarea id="orbisius_support_tickets_data_message"
+                                      class="orbisius_support_tickets_data_message form-control"
+                                      name="orbisius_support_tickets_data[message]"
+                                      placeholder="Please enter your message here..."
+                                      rows="<?php echo $row_num;?>"><?php esc_attr_e($data['message']); ?></textarea>
                         </div>
                     </div>
 
@@ -84,5 +105,15 @@ var_dump($_REQUEST);
 		ob_end_clean();
 
 		return $html;
+	}
+
+	/**
+     * @todo sanitize but allow some tags
+	 * @return array
+	 */
+	public function getData() {
+		$sanitized_data = empty($_REQUEST['orbisius_support_tickets_data']) ? [] : $_REQUEST['orbisius_support_tickets_data'];
+		$sanitized_data = array_map('trim', $sanitized_data);
+		return $sanitized_data;
 	}
 }
