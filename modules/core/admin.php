26559958a67f2c8e758b800f202020a8b45c27f7
Slavi <slavi@orbisius.com>
2019-01-13 04:02:05 +0200

    added ui & settings for notifications

--- modules/core/admin.php
+++ modules/core/admin.php
@@ -22,6 +22,13 @@ class Orbisius_Support_Tickets_Module_Core_Admin {
 		add_action( 'admin_menu', array( $this, 'addMenuPages' ) );
 		register_setting($this->plugin_settings_group_key, $this->plugin_settings_key, array($this, 'validateSettingsData'));
 
+		$settings_key = $this->plugin_settings_key;
+		$settings_group = $this->plugin_settings_group_key;
+
+		$notif_settings_key = $settings_key . '_notification';
+		$notif_settings_group = $settings_group . '_notification';
+		register_setting($notif_settings_group, $notif_settings_key, array($this, 'validateNotificationSettingsData'));
+
 		$ctx = [
 			'settings_key' => $this->plugin_settings_key,
 			'settings_group_key' => $this->plugin_settings_group_key,
@@ -50,6 +57,25 @@ class Orbisius_Support_Tickets_Module_Core_Admin {
 		return $input;
 	}
 
+	/**
+	 * This is called by WP after the user hits the submit button.
+	 * The variables are trimmed first and then passed to the who ever wantsto filter them.
+	 * @param array the entered data from the settings page.
+	 * @return array the modified input array
+	 */
+	function validateNotificationSettingsData($input) { // whitelist options
+		$ctx = [];
+		$req_obj = Orbisius_Support_Tickets_Request::getInstance();
+		$input = $req_obj->trim($input);
+
+		// let extensions do their thing
+		$input_filtered = apply_filters('orbisius_support_tickets_filter_admin_filter_settings', $input, $ctx);
+
+		// did an extension break data?
+		$input = is_array($input_filtered) ? $input_filtered : $input;
+
+		return $input;
+	}
 
 	/**
 	 * Add Menu item on WP Backend
@@ -273,6 +299,66 @@ class Orbisius_Support_Tickets_Module_Core_Admin {
                                             </p>
                                          </form>
                                     </p>
+
+                                </div>
+                                <!-- .inside -->
+
+                            </div>
+                            <!-- .postbox -->
+
+
+                            <?php
+                            $notif_settings_key = $settings_key . '_notification';
+                            $notif_settings_group = $settings_group . '_notification';
+                            $notif_opts = $this->getOptions($notif_settings_key);
+                            ?>
+                            <div class="postbox">
+
+                                <button type="button" class="handlediv" aria-expanded="true" >
+                                    <span class="screen-reader-text">Toggle panel</span>
+                                    <span class="toggle-indicator" aria-hidden="true"></span>
+                                </button>
+                                <!-- Toggle -->
+                                <h2 class="hndle"><span><?php esc_attr_e( 'Notification Settings', 'orbisius_support_tickets' ); ?></span></h2>
+
+                                <div class="inside">
+                                    <p>
+                                        <form method="post" action="options.php">
+		                                    <?php settings_fields($notif_settings_group); ?>
+                                            <table class="form-table">
+                                                <tr valign="top">
+                                                    <th scope="row">Ticket update subject</th>
+                                                    <td>
+                                                        <input type="text" class="widefat"
+                                                               name="<?php echo "{$notif_settings_key}[subject]";?>"
+                                                               value="<?php empty($notif_opts['subject']) ? '' : esc_attr_e($notif_opts['subject']);?>" />
+                                                    </td>
+                                                </tr>
+                                                <tr valign="top">
+                                                    <th scope="row">Ticket update message</th>
+                                                    <td>
+                                                        <textarea class="widefat"
+                                                               name="<?php echo "{$notif_settings_key}[message]";?>" rows="8"><?php
+                                                            empty($notif_opts['message']) ? '' : esc_attr_e($notif_opts['message']);?></textarea>
+                                                    </td>
+                                                </tr>
+                                                <tr valign="top">
+                                                    <th scope="row">Enable notification</th>
+                                                    <td>
+                                                        <input type="checkbox" class=""
+                                                               name="<?php echo "{$notif_settings_key}[enabled]";?>"
+                                                               <?php checked(empty($notif_opts['enabled']) ? '' : $notif_opts['enabled'], 1); ?>
+                                                               value="1" />
+                                                    </td>
+                                                </tr>
+                                            </table>
+
+                                            <p class="submit">
+                                                <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+                                            </p>
+                                         </form>
+                                    </p>
+
                                 </div>
                                 <!-- .inside -->
 
