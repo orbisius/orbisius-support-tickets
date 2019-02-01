1cf02856a52d0837bf23a06fb40218a22f27bae9
Slavi <slavi@orbisius.com>
2019-02-01 15:54:26 +0200

    added prepMergeTags method in modules/core/notifications.php to prep some common vars

--- modules/core/notifications.php
+++ modules/core/notifications.php
@@ -7,11 +7,13 @@ add_action('init', array( $notif_api, 'init' ) ) ;
 class Orbisius_Support_Tickets_Module_Core_Notifications {
 	public function init() {
 		//add_action( 'orbisius_support_tickets_admin_action_register_settings', array(  $this, 'notifyOnNewTicket' ) );
-		add_action( 'orbisius_support_tickets_action_before_submit_ticket_after_insert', array(  $this, 'notifyOnNewTicket' ) );
+		add_action( 'orbisius_support_tickets_action_submit_ticket_after_insert', array(  $this, 'notifyOnNewTicket' ) );
 		add_action( 'orbisius_support_tickets_action_ticket_activity', array( $this, 'notifyOnTicketActivity' ) );
 	}
 
 	/**
+	 * This email is sent to the ticket submitted so they can get a copy and know that we've received their ticket.
+	 * For now admin is BCC'ed so we can reuse the same email template.
 	 * @param array $ctx
 	 */
 	public function notifyOnNewTicket($ctx = array()) {
@@ -29,56 +31,45 @@ class Orbisius_Support_Tickets_Module_Core_Notifications {
 		$admin_api = Orbisius_Support_Tickets_Module_Core_Admin::getInstance();
 		$notif_key_in_opts = $admin_api->getPluginSettingsNotificationKey();
 		$notif_opts = $admin_api->getOptions($notif_key_in_opts);
+		$recipient_email = '';
 
 		if (empty($notif_opts['new_ticket_notification_enabled'])) {
 		    return;
         }
 
+		// Author of the reply
 		if (!empty($ctx['author_id'])) {
 			$user_id = $ctx['author_id'];
-
 			$user_obj = get_user_by('id', $user_id);
 
 			if (empty($user_obj)) { // user not found.
 				return;
 			}
 
-			$email = $user_obj->user_email;
-		} elseif (!empty($ctx['recipient_email'])) {
-			$email = $ctx['recipient_email'];
+			$recipient_email = $user_obj->user_email;
+		} elseif (!empty($ctx['recipient_email']) && is_email($ctx['recipient_email'])) {
+			$recipient_email = $ctx['recipient_email'];
+		} elseif (!empty($ctx['author_email']) && is_email($ctx['author_email'])) {
+			$recipient_email = $ctx['author_email'];
 		} else {
-			return '';
+			// Maybe it's a suggestion that doesn't require an email.
+			//return ''; // no email
 		}
 
-		$recipient_email = $email;
-
-		$host = 'localhost';
-
-		if (!empty($_SERVER['SERVER_NAME'])) {
-			$host = $_SERVER['SERVER_NAME'];
-		} elseif (!empty($_SERVER['HTTP_HOST'])) {
-			$host = $_SERVER['HTTP_HOST'];
-		} elseif (function_exists('shell_exec')) {
-			$host = shell_exec('hostname');
+		if (!empty($notif_opts['support_email_receiver'])) {
+			$support_email_receiver = $notif_opts['support_email_receiver'];
+		} else {
+			$support_email_receiver = get_option( 'admin_email' );
 		}
 
-		$host = wp_strip_all_tags($host);
-		$host = str_replace('www.', '', $host);
-		$host = trim($host);
+		$merge_tags['support_email_receiver'] = $support_email_receiver;
 
-		$shortcode_api = Orbisius_Support_Tickets_Module_Core_Shortcodes::getInstance();
-
-		$vars = array(
-			'domain' => $host,
-			'site_url' => site_url('/'),
-			'site_name' => get_bloginfo('name'),
-			'ticket_id' => $ctx['ticket_id'],
-			'ticket_password' => empty($ctx['ticket_pass']) ? '' : $ctx['ticket_pass'],
-			'ticket_url' => $shortcode_api->generateViewTicketLink( array( 'ticket_id' => $ctx['ticket_id'] ) ),
-			'recipient_email' => $email,
-		);
+		$vars = array_merge_recursive( $this->prepMergeTags($ctx), array(
+			'recipient_email' => $recipient_email,
+		));
 
 		$vars = apply_filters( 'orbisius_support_tickets_filter_notification_replace_vars', $vars, $ctx );
+		$host = $vars['host'];
 
 		// subject
 		$subject = $notif_opts['new_ticket_subject'];
@@ -102,7 +93,7 @@ class Orbisius_Support_Tickets_Module_Core_Notifications {
 
 		if (!empty($notif_opts['support_email_reply_to'])) {
 			$reply_to_full_email = $notif_opts['support_email_reply_to'];
-		} elseif (!empty($notif_opts['support_email_receiver'])) {
+		} elseif (!empty($notif_opts['support_email_receiver']) && $from_email != $notif_opts['support_email_receiver']) {
 			$reply_to_full_email = $notif_opts['support_email_receiver'];
 		}
 
@@ -116,16 +107,16 @@ class Orbisius_Support_Tickets_Module_Core_Notifications {
 		$attachments = array();
 		$attachments = apply_filters( 'orbisius_support_tickets_filter_notification_email_attachments', $attachments, $ctx );
 
-		// For now let's BCC the admin
-		if (!empty($notif_opts['support_email_receiver'])) {
-			$headers[] = "Bcc: " . $notif_opts['support_email_receiver'];
+		if (empty($recipient_email)) {
+			$subject = '[no_client_email] ' . $subject;
+			$recipient_email = $support_email_receiver;
+		} elseif ( ! empty( $support_email_receiver ) ) { // For now let's BCC the admin
+			$headers[] = "Bcc: " . $support_email_receiver;
 		}
 
 		$mail_sent_status = wp_mail($recipient_email, $subject, $message, $headers, $attachments);
 
-		// @todo 1 email to admin or just BCC?
-//		$subject = $notif_opts['new_ticket_subject_admin'];
-//		$message = $notif_opts['new_ticket_message_admin'];
+		return $mail_sent_status;
 	}
 
 	/**
@@ -146,15 +137,15 @@ class Orbisius_Support_Tickets_Module_Core_Notifications {
 
 		$admin_api = Orbisius_Support_Tickets_Module_Core_Admin::getInstance();
 		$notif_key_in_opts = $admin_api->getPluginSettingsNotificationKey();
-		$notif_opts = $admin_api->getOptions($notif_key_in_opts);
+		$notif_opts = $admin_api->getOptions($notif_key_in_opts); // @todo use a const about NOTIF section
 
 		if (empty($notif_opts['ticket_activity_notification_enabled'])) {
 		    return;
         }
 
+		// @todo decide the context. admin -> posts a reply OR client posts a reply.
 		if (!empty($ctx['author_id'])) {
 			$user_id = $ctx['author_id'];
-
 			$user_obj = get_user_by('id', $user_id);
 
 			if (empty($user_obj)) { // user not found.
@@ -168,25 +159,21 @@ class Orbisius_Support_Tickets_Module_Core_Notifications {
 			return '';
 		}
 
-		$host = 'localhost';
+		$user_api = Orbisius_Support_Tickets_User::getInstance();
 
-		if (!empty($_SERVER['SERVER_NAME'])) {
-			$host = $_SERVER['SERVER_NAME'];
-		} elseif (!empty($_SERVER['HTTP_HOST'])) {
-			$host = $_SERVER['HTTP_HOST'];
-		} elseif (function_exists('shell_exec')) {
-			$host = shell_exec('hostname');
+		if (!empty($notif_opts['support_email_receiver'])) {
+			$recipient_email = $notif_opts['support_email_receiver'];
+		} else {
+			$recipient_email         = get_option( 'admin_email' );
 		}
 
-		$host = wp_strip_all_tags($host);
-		$host = str_replace('www.', '', $host);
-		$host = trim($host);
+		$event_type = 'client_reply';
 
-		$user_api = Orbisius_Support_Tickets_User::getInstance();
-		$recipient_email = $notif_opts['support_email_receiver'];
-
-		// support posts a reply -> notify client
-		if ($user_api->isEditor($ctx['author_id'])) {
+		if (empty($ctx['author_id']) || ! is_user_logged_in()) { // not logged in user
+			$event_type = 'client_reply';
+		}
+		// Support posts a reply -> notify client
+		elseif ($user_api->isSupportRep($ctx['author_id'])) {
 			if (!empty($ticket_obj->post_author)) {
 				$client_obj = $user_api->getUser( $ticket_obj->post_author ); // Find who created the ticket.
 
@@ -194,30 +181,28 @@ class Orbisius_Support_Tickets_Module_Core_Notifications {
 					$recipient_email = $client_obj->user_email;
 				}
 			}
+
+			$event_type = 'support_reply';
 		}
 
-		$shortcode_api = Orbisius_Support_Tickets_Module_Core_Shortcodes::getInstance();
-
-		$vars = array(
-			'domain' => $host,
-			'site_url' => site_url('/'),
-			'site_name' => get_bloginfo('name'),
-			'ticket_id' => $ctx['ticket_id'],
-			'ticket_url' => $shortcode_api->generateViewTicketLink( array( 'ticket_id' => $ctx['ticket_id'] ) ),
+		$vars = array_merge_recursive( $this->prepMergeTags($ctx), array(
 			'recipient_email' => $recipient_email,
-		);
+		));
 
 		$vars = apply_filters( 'orbisius_support_tickets_filter_notification_replace_vars', $vars, $ctx );
+		$host = $vars['host'];
 
 		// subject
 		$subject = $notif_opts['ticket_activity_subject'];
 		$subject = do_shortcode($subject);
 		$subject = Orbisius_Support_Tickets_String_Util::replaceVars($subject, $vars);
+		$subject = apply_filters( 'orbisius_support_tickets_filter_notification_ticket_activity_email_subject', $subject, $ctx );
 
 		// message
 		$message = $notif_opts['ticket_activity_message'];
 		$message = do_shortcode($message);
 		$message = Orbisius_Support_Tickets_String_Util::replaceVars($message, $vars);
+		$message = apply_filters( 'orbisius_support_tickets_filter_notification_ticket_activity_email_message', $message, $ctx );
 		//$message = nl2br($message);
 		$headers = array();
 
@@ -231,7 +216,7 @@ class Orbisius_Support_Tickets_Module_Core_Notifications {
 
 		if (!empty($notif_opts['support_email_reply_to'])) {
 			$reply_to_full_email = $notif_opts['support_email_reply_to'];
-		} elseif (!empty($notif_opts['support_email_receiver'])) {
+		}  elseif (!empty($notif_opts['support_email_receiver']) && $from_email != $notif_opts['support_email_receiver']) {
 			$reply_to_full_email = $notif_opts['support_email_receiver'];
 		}
 
@@ -250,6 +235,45 @@ class Orbisius_Support_Tickets_Module_Core_Notifications {
 		if (empty($notif_opts['support_email_receiver'])) {
 			return;
 		}
+	}
+
+	/**
+	 * @param array $params
+	 * @return array
+	 */
+	public function prepMergeTags($ctx = array()) {
+		$shortcode_api = Orbisius_Support_Tickets_Module_Core_Shortcodes::getInstance();
+
+		$host = 'localhost';
+
+		if (!empty($_SERVER['SERVER_NAME'])) {
+			$host = $_SERVER['SERVER_NAME'];
+		} elseif (!empty($_SERVER['HTTP_HOST'])) {
+			$host = $_SERVER['HTTP_HOST'];
+		} elseif (function_exists('shell_exec')) {
+			$host = shell_exec('hostname');
+		}
+
+		$host = wp_strip_all_tags($host);
+		$host = str_replace('www.', '', $host);
+		$host = trim($host);
+
+		$merge_tags = array(
+			'host' => $host,
+			'domain' => $host,
+			'site_url' => site_url('/'),
+			'site_name' => get_bloginfo('name'),
+			'ticket_id' => 0,
+			'ticket_url' => '#',
+			'ticket_password' => empty($ctx['ticket_pass']) ? '' : $ctx['ticket_pass'],
+		);
+
+		if (!empty($ctx['ticket_id'])) {
+			$merge_tags['ticket_id'] = $ctx['ticket_id'];
+			$merge_tags['ticket_url'] = $shortcode_api->generateViewTicketLink( array( 'ticket_id' => $ctx['ticket_id'] ) );
+		}
+
+		return $merge_tags;
 	}
 
 	/**
