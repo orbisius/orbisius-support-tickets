fa96a841383ef202dce288864c34e64ef0e5f42b
Hector <6560172+hector-codes@users.noreply.github.com>
2020-12-02 23:58:27 +0330

    Easier test mode enabling
    It is common in WP to add extra config in config.php to enable/disable features

--- orbisius-support-tickets.php
+++ orbisius-support-tickets.php
@@ -1,31 +1,31 @@
 <?php
-/*
-  Plugin Name: Orbisius Support Tickets
-  Plugin URI: https://orbisius.com/products/wordpress-plugins/orbisius-support-tickets
-  Description: Minimalistic support ticket system that enables you to start providing awesome support in 2 minutes.
-  Version: 1.0.3
-  Author: Svetoslav Marinov (Slavi) | Orbisius.com
-  Author URI: http://orbisius.com
-  Text Domain: orbisius_support_tickets
-  Domain Path: /lang
+/**
+ * Plugin Name: Orbisius Support Tickets
+ * Plugin URI: https://orbisius.com/products/wordpress-plugins/orbisius-support-tickets
+ * Description: Minimalistic support ticket system that enables you to start providing awesome support in 2 minutes.
+ * Version: 1.0.3
+ * Author: Svetoslav Marinov (Slavi) | Orbisius.com
+ * Author URI: http://orbisius.com
+ * Text Domain: orbisius_support_tickets
+ * Domain Path: /lang
  */
 
-define('ORBISIUS_SUPPORT_TICKETS_BASE_PLUGIN', __FILE__);
-define('ORBISIUS_SUPPORT_TICKETS_BASE_DIR', dirname(__FILE__));
-define('ORBISIUS_SUPPORT_TICKETS_BASE_URL', plugins_url( '', ORBISIUS_SUPPORT_TICKETS_BASE_PLUGIN ));
-define('ORBISIUS_SUPPORT_TICKETS_DATA_DIR', ORBISIUS_SUPPORT_TICKETS_BASE_DIR . '/data');
-define('ORBISIUS_SUPPORT_TICKETS_SHARE_DIR', ORBISIUS_SUPPORT_TICKETS_BASE_DIR . '/share');
-define('ORBISIUS_SUPPORT_TICKETS_DEV_ENV', !empty($_SERVER['DEV_ENV'])
-                         || (!empty($_SERVER['HTTP_HOST']) && preg_match('#localhost|devel\.ca|qsandbox|\.clients\.|staging#si', $_SERVER['HTTP_HOST'])));
-define('ORBISIUS_SUPPORT_TICKETS_LIVE_ENV', !ORBISIUS_SUPPORT_TICKETS_DEV_ENV);
+define( 'ORBISIUS_SUPPORT_TICKETS_BASE_PLUGIN', __FILE__ );
+define( 'ORBISIUS_SUPPORT_TICKETS_BASE_DIR', dirname( __FILE__ ) );
+define( 'ORBISIUS_SUPPORT_TICKETS_BASE_URL', plugins_url( '', ORBISIUS_SUPPORT_TICKETS_BASE_PLUGIN ) );
+define( 'ORBISIUS_SUPPORT_TICKETS_DATA_DIR', ORBISIUS_SUPPORT_TICKETS_BASE_DIR . '/data' );
+define( 'ORBISIUS_SUPPORT_TICKETS_SHARE_DIR', ORBISIUS_SUPPORT_TICKETS_BASE_DIR . '/share' );
 
-$libs = glob(ORBISIUS_SUPPORT_TICKETS_BASE_DIR . '/lib/*.php');
-$mods = glob(ORBISIUS_SUPPORT_TICKETS_BASE_DIR . '/modules/*/*.php');
+// Set the following to false to enable dev mode.
+define( 'ORBISIUS_SUPPORT_TICKETS_LIVE_ENV', true );
 
-$libs = array_merge((array) $libs, (array) $mods);
-$libs = array_unique($libs);
-$libs = array_filter($libs);
+$libs = glob( ORBISIUS_SUPPORT_TICKETS_BASE_DIR . '/lib/*.php' );
+$mods = glob( ORBISIUS_SUPPORT_TICKETS_BASE_DIR . '/modules/*/*.php' );
 
-foreach ($libs as $lib_file) {
-	require_once( $lib_file );
+$libs = array_merge( (array) $libs, (array) $mods );
+$libs = array_unique( $libs );
+$libs = array_filter( $libs );
+
+foreach ( $libs as $lib_file ) {
+	require_once $lib_file;
 }
