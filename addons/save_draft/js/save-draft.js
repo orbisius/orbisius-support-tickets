e9737baad5f36ba07f363b5e0253e577f9f11a1a
Petar Petrov <pdpetrov98@gmail.com>
2024-04-19 21:12:56 +0300

    Added local storage saving to replies

--- addons/save_draft/js/save-draft.js
+++ addons/save_draft/js/save-draft.js
@@ -1,5 +1,8 @@
 jQuery(document).ready(function ($) {
-    function saveStorage() {
+    // save the typed data to local storage
+
+    // for tickets
+    function saveTicketStorage() {
         let data = {
             subject: $('#orbisius_support_tickets_data_subject').val(),
             message: $('#orbisius_support_tickets_data_message').val(),
@@ -8,7 +11,7 @@ jQuery(document).ready(function ($) {
         localStorage.setItem('ticketData', JSON.stringify(data));
     }
 
-    function loadStorage() {
+    function loadTicketStorage() {
         let savedData = localStorage.getItem('ticketData');
         if (savedData) {
             savedData = JSON.parse(savedData);
@@ -25,11 +28,44 @@ jQuery(document).ready(function ($) {
         }
     }
 
-    setInterval(saveStorage, 5000);
+    setInterval(saveTicketStorage, 5000);
 
     $('#orbisius_support_tickets_submit_ticket_form').submit(function () {
         localStorage.removeItem('ticketData');
     });
 
-    loadStorage();
+    loadTicketStorage();
+
+    // for comments
+    function saveCommentStorage() {
+        let data = {
+            comment: $('#comment').val(),
+            timestamp: new Date().getTime()
+        };
+        localStorage.setItem('commentData', JSON.stringify(data));
+    }
+
+    function loadCommentStorage() {
+        let savedData = localStorage.getItem('commentData');
+        if (savedData) {
+            savedData = JSON.parse(savedData);
+            let currentTime = new Date().getTime();
+            let timeDiff = (currentTime - savedData.timestamp) / 1000;
+
+            // saved data is less than 7 days old (just an example, we might want to set it to clear after 7 days)
+            if (timeDiff < 604800) {
+                $('#comment').val(savedData.comment);
+            } else {
+                localStorage.removeItem('commentData');
+            }
+        }
+    }
+
+    setInterval(saveCommentStorage, 5000);
+
+    $('#commentform').submit(function () {
+        localStorage.removeItem('commentData');
+    });
+
+    loadCommentStorage();
 });
